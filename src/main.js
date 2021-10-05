7f95605d65cce64af64b4047239accb569a42dca
Jacob-tu <hututu13@foxmail.com>
2021-10-05 13:09:49 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import router from './router'
 import './plugins/element.js'
 //导入全局样式表
 import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
 //配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios 请求拦截
+axios.interceptors.request.use(config => {
+  //为请求头对象添加 Token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
